eccb7e9bc7ef805d4094e4b18e037608b987f160
justcabyr <asulaiman@dervac.com>
2023-10-18 21:56:27 -0230

    pure react components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const House = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.type),
+    React.createElement('h2', {}, props.bed),
+    React.createElement('h2', {}, props.bath),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Buy a house!'),
+    React.createElement(House, {
+      type: 'apartment',
+      bed: 'two',
+      bath: 'two',
+    }),
+    React.createElement(House, {
+      type: 'house',
+      bed: 'three',
+      bath: 'two',
+    }),
+    React.createElement(House, {
+      type: 'apartment',
+      bed: 'five',
+      bath: 'three',
+    }),
+  ]);
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
