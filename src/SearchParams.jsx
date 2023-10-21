56568893a82b42bc607bc583c8aa50e367465776
justcabyr <asulaiman@dervac.com>
2023-10-20 22:35:59 -0230

    adding house beds and using states

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import House from "./House";
 
-const HOUSES = ["apartment", "town house", "duplex", "single house", "studio"];
+const HOUSES = ["apartment", "town-house", "duplex", "single-house", "studio"];
 
 const SeachParams = () => {
   const [location, setLocation] = useState("");
   const [house, setHouse] = useState("");
+  const [bed, setBed] = useState("");
+  const [homes, setHomes] = useState([]);
+  const beds = [];
+
+  useEffect(() => {
+    requestHomes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestHomes() {
+    const res = await fetch(
+      `url`,
+    );
+
+    const json = await res.json();
+
+    setHomes(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestHomes();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -25,16 +49,48 @@ const SeachParams = () => {
             value={house}
             onChange={(e) => {
               setHouse(e.target.value);
+              setBed("");
+            }}
+            onBlur={(e) => {
+              setHouse(e.target.value);
+              setBed("");
             }}
           >
             <option />
-            {HOUSES.map((house) => {
-              <option key={house}>{house}</option>;
+            {HOUSES.map((house) => (
+              <option key={house} value={house}>
+                {house}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="bed">
+          Bed
+          <select
+            id="bed"
+            disabled={beds.length === 0}
+            value={bed}
+            onChange={(e) => {
+              setBed(e.target.value);
+            }}
+          >
+            <option />
+            {beds.map((bed) => {
+              <option key={bed}>{bed}</option>;
             })}
           </select>
         </label>
         <button>Submit</button>
       </form>
+
+      {homes.map((home) => (
+        <House
+          type={home.type}
+          bed={home.bed}
+          bath={home.bath}
+          key={home.id}
+        />
+      ))}
     </div>
   );
 };
